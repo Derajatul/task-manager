ecd4c9d67b7baf2599eeec46c11f97069b642ff5
Moh Derajatul Ula <derajatul@gmail.com>
2025-06-28 23:29:54 +0700

    Refactor Prisma schema and migrations; remove unused models and seed data
    - Updated `package.json` to remove unused database commands and dependencies.
    - Deleted old migration files and created a new migration for the `Task` model.
    - Refactored `schema.prisma` to simplify the `Task` model and remove unused models and enums.
    - Removed the seed script as it is no longer needed.
    - Deleted the Prisma client initialization file.
    - Updated TypeScript configuration to adjust module paths.

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,6 @@
-// import prisma from "@/lib/prisma";
 import Image from "next/image";
 
-export default async function Home() {
-  // const users = await prisma.user.findMany();
-  // find task john doe
-  // const johnDoe = await prisma.user.findFirst({
-  //   where: { name: "John Doe" },
-  //   include: {
-  //     tasks: {
-  //       include: {
-  //         tags: true,
-  //       },
-  //     },
-  //   },
-  // });
-
-  // return <pre>{JSON.stringify(johnDoe, null, 2)}</pre>;
+export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -31,7 +16,7 @@ export default async function Home() {
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
+              app/page.tsx
             </code>
             .
           </li>
